bdbad963bb3f1c68639738080a3782b77431cfba
Buivalo Anastasia <abuyvalo@mail.ru>
2024-02-07 12:21:06 +0300

    add confirm.accept

---
+++ test_confirm.py
@@ -0,0 +1,32 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+import time
+import math
+
+def calc(x):
+  return str(math.log(abs(12*math.sin(int(x)))))
+try: 
+    link = "http://suninjuly.github.io/alert_accept.html"
+    browser = webdriver.Chrome()
+    browser.get(link)
+
+    btn = browser.find_element(By.CSS_SELECTOR, "button")
+    btn.click()
+
+    confirm = browser.switch_to.alert
+    confirm.accept()
+
+    x_element = browser.find_element(By.ID, "input_value")
+    x = x_element.text
+    y = calc(x)
+    result= browser.find_element(By.ID, "answer")
+    result.send_keys(y)
+    
+    btn = browser.find_element(By.CSS_SELECTOR, "button")
+    btn.click()
+
+finally:
+    # успеваем скопировать код за 30 секунд
+    time.sleep(20)
+    # закрываем браузер после всех манипуляций
+    browser.quit()
